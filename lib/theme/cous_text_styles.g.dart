7c6186304103ce2d9dece01cfea05e61544468ed
Parabeac-Nest <admin@parabeac.com>
2022-11-08 18:12:02 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/cous_text_styles.g.dart
@@ -0,0 +1,60 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class CousTextStyles {
+
+static const TextStyle headingH4 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -0.5,
+  fontFamily: 'Nunito Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle paragraphLarge = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.5,
+  fontFamily: 'Nunito Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle paragraphLargeBold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w800,
+  letterSpacing: -0.5,
+  fontFamily: 'Nunito Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH5 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  letterSpacing: -0.5,
+  fontFamily: 'Nunito Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle paragraphSmallBold = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w800,
+  letterSpacing: 0,
+  fontFamily: 'Nunito Sans',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
